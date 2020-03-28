d49618992841d1c978a4665627d8d11476a04aaa
Shubhang <shubhang.verma_cs18@gla.ac.in>
2020-03-28 14:11:31 +0530

    completing addAllPreOrderSuccessor in Queue.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -6,9 +6,13 @@
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
+import com.sun.source.tree.Tree;
 import problem1.mybst.MyBinarySearchTree;
 import problem1.node.TreeNode;
 import problem4.element.Element;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MyQueue {
     private Element front;
@@ -31,7 +35,9 @@ public class MyQueue {
     }
 
     public void addAllPreOrderSuccessor(){
-
+        TreeNode current = tree.getRoot();
+        List l = new LinkedList();
+        l.add(current);
     }
 
     public boolean isEmpty(){
