5b8cd4b35047b67491bb65e454d91a9b2f05841e
Shubhang <shubhang.verma_cs18@gla.ac.in>
2020-03-28 10:31:46 +0530

    creating insertHelper method for BST

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -20,7 +20,35 @@ public class MyBinarySearchTree {
             //System.out.println("root Node inserted");
         }
         else {
-            //insertHelper(root,value);
+            insertHelper(root,value);
         }
+    }
+
+    private void insertHelper(TreeNode current,int value){
+        TreeNode node = new TreeNode(value);
+        if(value == current.getData()){
+            return;
+        }
+        else if(value < current.getData()){
+            if(current.getLeftChild() == null){
+                //System.out.println("New Node inserted");
+                current.setLeftChild(node);
+                node.setParent(current);
+            }
+            else {
+                insertHelper(current.getLeftChild(),value);
+            }
+        }
+        else {
+            if(current.getRightChild() == null){
+                //System.out.println("New Node inserted");
+                current.setRightChild(node);
+                node.setParent(current);
+            }
+            else {
+                insertHelper(current.getRightChild(),value);
+            }
+        }
+        return;
     }
 }
