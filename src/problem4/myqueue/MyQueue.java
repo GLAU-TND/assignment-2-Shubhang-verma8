d8239eff25686aa78d72229c5bb1121c26f99ea0
Shubhang <shubhang.verma_cs18@gla.ac.in>
2020-03-28 13:58:59 +0530

    creating Queue class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,7 +8,8 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 import problem1.mybst.MyBinarySearchTree;
 import problem1.node.TreeNode;
+import problem4.element.Element;
 
 public class MyQueue {
-
+    //private Element
 }
